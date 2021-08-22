a0a233f2e7ea070b57711b0b1a731e404ead851e
Mattie Schraeder <mattie@mattie.lgbt>
2021-08-22 05:24:57 -0500

    get all channels

--- modules/Channels.js
+++ modules/Channels.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { Config } from "../config.js";
 
 export default function createRouter(sql) {
-  const router = express.Router(); 
+  const router = express.Router();
 
   //Get all categories
   router.get('/api/categories', function (req, res, next) {
@@ -11,7 +11,7 @@ export default function createRouter(sql) {
     sql.getSession()
     .then(s => { session = s; return session.getSchema(Config.MYSQL_ARCHIVESDB) })
     .then(s => {
-        return session.sql("SELECT DISTINCT category FROM " + Config.MYSQL_ARCHIVESDB + ".channels ORDER BY category ASC").execute().catch(err => {            
+        return session.sql("SELECT DISTINCT category FROM " + Config.MYSQL_ARCHIVESDB + ".channels ORDER BY category ASC").execute().catch(err => {
             res.status(500).json({status: err});
         })
     })
@@ -48,10 +48,34 @@ export default function createRouter(sql) {
     })
   });
 
+  //Get all channels
+  router.get('/api/channels', function (req, res, next) {
+    var session;
+
+    sql.getSession()
+    .then(s => { session = s; return session.getSchema(Config.MYSQL_ARCHIVESDB) })
+    .then(s => { return s.getTable("channels") })
+    .then(t =>
+        t.select()
+        .orderBy("channel")
+        .bind("category", req.params.category)
+        .execute()
+        .catch(err => {
+            res.status(500).json({status: err});
+        }))
+    .then(r => {
+        if (r) {
+            var output = r.fetchAll();
+
+            res.status(200).json(output);
+        }
+    })
+  });
+
   //Get all messages for a channel
   router.get('/api/messages/channel/:channelId', function (req, res, next) {
     var session;
-    
+
     sql.getSession()
     .then(s => { session = s; return session.getSchema(Config.MYSQL_ARCHIVESDB) })
     .then(s => { return s.getTable("messages") })
