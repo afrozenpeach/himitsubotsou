e09bc54ab9cf9480066229f5ce90e1788e9c3672
Mattie Schraeder <mattie@mattie.lgbt>
2021-09-02 22:16:52 -0500

    Add fic db to the channels

--- modules/Channels.js
+++ modules/Channels.js
@@ -496,5 +496,49 @@ export default function createRouter(sql) {
     });
   });
 
+  //Get all fics
+  router.get('/api/fics', function (req, res, next) {
+    let session;
+    let result = [];
+    let results = [];
+
+    sql.getSession()
+    .then(s => { session = s; return session.getSchema(Config.MYSQL_FICDB) })
+    .then(s => { return s.getTable("fics") })
+    .then(t =>
+        t.select()
+        .orderBy(['year asc', 'month asc', 'day asc'])
+        .execute(
+            row => {
+                row.forEach((value, i) => {
+                    result[i] = Object.assign({}, result[i], { value });
+                });
+
+                let resultCopy = [...result];
+                results.push(resultCopy);
+            },
+            columns => {
+                columns.forEach((key, i) => { result[i] = Object.assign({}, result[i], { key: key.getColumnName() }) });
+            }
+        )
+        .catch(err => {
+            res.status(500).json({status: err});
+        }))
+    .then(() => {
+        let output = [];
+
+        for (const r of results) {
+            let npc = r.reduce((res, pair) => Object.assign(res, { [pair.key]: pair.value }), {});
+            output.push(npc);
+        }
+
+        res.status(200).json(output);
+        session.close();
+    })
+    .catch(e => {
+        res.status(503).json("API Error");
+    });
+  });
+
   return router;
 }
