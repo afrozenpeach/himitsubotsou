62b602cefddbcbac387e0539396379369b39fa21
Mattie Schraeder <mattie@mattie.lgbt>
2021-09-02 20:45:04 -0500

    Add all messages so that's searchable

--- modules/Channels.js
+++ modules/Channels.js
@@ -31,6 +31,7 @@ export default function createRouter(sql) {
   //Get all channels for a category
   router.get('/api/channels/category/:category', function (req, res, next) {
     let session;
+    let session2;
 
     if (req.params.category === 'Archive - All Channels') {
         req.params.category = '%';
@@ -52,48 +53,69 @@ export default function createRouter(sql) {
         if (r) {
             let fetched = r.fetchAll();
             let output = [];
+            let promises = [];
 
-            fetched.forEach(f => {
-                let d = f[2].split('_');
-
-                try {
-                    output.push({
-                        id: f[0],
-                        category: f[1],
-                        channel: f[2],
-                        location: d[0].replaceAll('-', ' ').split(' ').map(function(word) {
-                            return (word.charAt(0).toUpperCase() + word.slice(1));
-                        }).join(' '),
-                        characters: d[1].split('-').map(function(word) {
-                            return (word.charAt(0).toUpperCase() + word.slice(1));
-                        }),
-                        date: new Date(d[2].replace('-ar', '').replace('ar', '')),
-                        discordId: f[3]
+            for (const f of fetched) {
+                promises.push(
+                    sql.getSession()
+                    .then(s2 => { session2 = s2; return session2.getSchema(Config.MYSQL_ARCHIVESDB)})
+                    .then(() => {
+                        return session2.sql("SET SESSION group_concat_max_len = 1000000;").execute().catch();
                     })
-                } catch (error) {
-                    output.push({
-                        id: f[0],
-                        category: f[1],
-                        channel: f[2]
+                    .then(() => {
+                        return session2.sql("SELECT GROUP_CONCAT(content SEPARATOR ', ') FROM " + Config.MYSQL_ARCHIVESDB + ".messages WHERE channelId = " + f[0] + " GROUP BY channelId").execute().catch(err => {
+                            res.status(500).json({status: err});
+                        })
                     })
-                }
-            });
+                    .then(r2 => {
+                        let d = f[2].split('_');
+                        let m = r2.fetchAll();
 
-            res.status(200).json(output.sort((a, b) => {
-                if (a.date > b.date) {
-                    return 1;
-                } else if (a.date === b.date) {
-                    if (a.id > b.id) {
+                        try {
+                            output.push({
+                                id: f[0],
+                                category: f[1],
+                                channel: f[2],
+                                location: d[0].replaceAll('-', ' ').split(' ').map(function(word) {
+                                    return (word.charAt(0).toUpperCase() + word.slice(1));
+                                }).join(' '),
+                                characters: d[1].split('-').map(function(word) {
+                                    return (word.charAt(0).toUpperCase() + word.slice(1));
+                                }),
+                                date: new Date(d[2].replace('-ar', '').replace('ar', '')),
+                                discordId: f[3],
+                                messages: m[0][0]
+                            })
+                        } catch (error) {
+                            output.push({
+                                id: f[0],
+                                category: f[1],
+                                channel: f[2]
+                            })
+                        }
+
+                        session2.close();
+                    })
+                );
+            }
+
+            Promise.all(promises).then(() => {
+                res.status(200).json(output.sort((a, b) => {
+                    if (a.date > b.date) {
                         return 1;
+                    } else if (a.date === b.date) {
+                        if (a.id > b.id) {
+                            return 1;
+                        } else {
+                            return -1;
+                        }
                     } else {
                         return -1;
                     }
-                } else {
-                    return -1;
-                }
-            }));
+                }));
 
-            session.close();
+                session.close();
+            })
         }
     })
     .catch(e => {
